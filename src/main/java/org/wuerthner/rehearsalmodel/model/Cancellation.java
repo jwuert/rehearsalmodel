a42da3e820efa0c1cb3a8145b6b2e1eeadf71cf4
Jan Würthner <jan.wuerthner@gmx.de>
2025-11-18 13:57:45 +0100

    added 'allowAcceptance' to Appointment; added 'acception' to Cancellation

--- src/main/java/org/wuerthner/rehearsalmodel/model/Cancellation.java
+++ src/main/java/org/wuerthner/rehearsalmodel/model/Cancellation.java
@@ -1,5 +1,6 @@
 package org.wuerthner.rehearsalmodel.model;
 
+import org.wuerthner.sport.attribute.BooleanAttribute;
 import org.wuerthner.sport.attribute.StringAttribute;
 import org.wuerthner.sport.core.AbstractModelElement;
 
@@ -11,7 +12,10 @@ public class Cancellation extends AbstractModelElement {
     public static final StringAttribute userid = new StringAttribute("userid")
             .label("ID");
 
+    public static final BooleanAttribute acception = new BooleanAttribute("acception")
+            .label("Zusage");
+
     public Cancellation() {
-        super(TYPE, Arrays.asList(), Arrays.asList(userid));
+        super(TYPE, Arrays.asList(), Arrays.asList(userid, acception));
     }
 }
