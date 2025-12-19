429f02aa62ee983345543f2b73beccb16aac0ad4
Jan Würthner <jan.wuerthner@gmx.de>
2025-12-19 14:57:48 +0100

    added project attribute to work

--- src/main/java/org/wuerthner/rehearsalmodel/model/Work.java
+++ src/main/java/org/wuerthner/rehearsalmodel/model/Work.java
@@ -1,8 +1,11 @@
 package org.wuerthner.rehearsalmodel.model;
 
+import org.wuerthner.sport.attribute.DynamicSelectableStringAttribute;
 import org.wuerthner.sport.attribute.IdAttribute;
 import org.wuerthner.sport.attribute.StringAttribute;
+import org.wuerthner.sport.check.True;
 import org.wuerthner.sport.core.AbstractModelElement;
+import org.wuerthner.sport.core.ElementFilter;
 
 import java.util.Arrays;
 
@@ -17,7 +20,12 @@ public class Work  extends AbstractModelElement {
     public static final StringAttribute composer = new StringAttribute("composer")
             .label("Komponist");
 
+    public static final DynamicSelectableStringAttribute project = new DynamicSelectableStringAttribute("project")
+            .label("Projekt")
+            .addFilter(new ElementFilter(Project.TYPE, new True()))
+            ;
+
     public Work() {
-        super(TYPE, Arrays.asList(Voice.TYPE), Arrays.asList(id, composer));
+        super(TYPE, Arrays.asList(Voice.TYPE), Arrays.asList(id, composer, project));
     }
 }
